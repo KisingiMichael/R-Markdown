0bff089a62c3b818332c93d14d026b6eb4d8c032
Michael Kisingi <kisingimichael12@gmail.com>
2021-04-30 22:44:39 +0300

    Add some R Scripts

---
+++ DataStructures.R
@@ -0,0 +1,32 @@
+#Data Structures in r programming
+
+**Data Structure**
+
+```{r}
+list
+Numeric
+logical
+characters
+```
+  
+#1. Lists
+my_list <- list(1, "aba", TRUE, 1 + 4i)
+my_list
+typeof(my_list);
+xlist
+##named list
+xlist <- list(a = "Karthik Ram", b = 1:10, data = head(mtcars))
+xlist
+#2.characters
+x <-"abacy"
+x
+#3.Numeric
+y <- 1:10
+y
+#4.logical
+x <- TRUE
+y <- FALSE
+x;y
+typeof(x);typeof(y)
+typeof(x);typeof(y)
+
